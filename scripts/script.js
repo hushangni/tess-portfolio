262ed6ec5e3358d41d12c2438d507ae59b780daa
Shangni Hu <shangnihu@MacBook-Pro.local>
2020-11-11 17:01:40 -0500

    Finished adding the content.

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,9 @@ $(() => {
   // Mobile Icon
   $('.mobile-icon').on('click', (e) => {
     $('.overall-nav').toggleClass('active');
-  })
+  });
+
+  $('.overall-nav a').on('click', function(e){
+    $('.overall-nav').toggleClass('active');
+})
 });
